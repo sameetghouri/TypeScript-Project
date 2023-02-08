784a278465f6b01dc6412798147abe819b1d6c4e
Sameet <sameetghouri@yahoo.com>
2023-02-08 15:49:09 +0500

    modified

--- sandbox.js
+++ sandbox.js
@@ -25,3 +25,42 @@ ninja.name = 'ryu'; //only assign string
 //ninja.skill= ['fighting','swimming'] -- can not assign a new property to a object
 ninja = { name: 'yoshi', belt: 'blue', age: 25 };
 //you can assign the object with the updated property values but properties must be same
+//Explicit types
+var chara;
+var ag;
+var isloggedin;
+ag = 30;
+isloggedin = true;
+//arrays
+var ninjas = []; //must initialize it with the empty array otherwise array.push won't work 
+ninjas = ['yoshi', 'mario'];
+//union types
+var mix = [];
+mix.push('hello');
+mix.push(20);
+mix.push(false);
+console.log(mix);
+var uid;
+uid = "124";
+uid = 124;
+//objects
+var ninjaone;
+ninjaone = { name: "yoshi", age: 40 };
+var ninjatwo;
+ninjatwo = { name: 'mario', age: 23, belt: 'black' };
+//Dynamic Values
+var agv = 25;
+agv = true;
+console.log(agv);
+agv = 'hello';
+console.log(agv);
+var mix2 = [];
+mix2.push(75);
+mix2.push('mario');
+mix2.push(false);
+console.log(mix2);
+var ninjas2;
+ninjas2 = { name: 'yoshi', age: 25 };
+console.log(ninjas2);
+ninjas2 = { name: 25, age: 'yoshi' };
+console.log(ninjas2);
--- sandbox.ts
+++ sandbox.ts
@@ -31,3 +31,59 @@ ninja.name='ryu'; //only assign string
 //ninja.skill= ['fighting','swimming'] -- can not assign a new property to a object
 ninja ={name:'yoshi',belt:'blue', age:25}
 //you can assign the object with the updated property values but properties must be same
+
+//Explicit types
+let chara:string;
+let ag:number;
+let isloggedin:boolean; 
+
+ag=30;
+isloggedin=true;
+
+//arrays
+let ninjas:string[] = [] //must initialize it with the empty array otherwise array.push won't work 
+ninjas=['yoshi','mario']
+
+//union types
+let  mix:(string|number|boolean)[]=[];
+mix.push('hello')
+mix.push(20)
+mix.push(false)
+console.log(mix)
+
+let uid: string|number;
+uid="124";
+uid=124;
+
+//objects
+let ninjaone: object;
+ninjaone={name:"yoshi",age:40}
+
+let ninjatwo:{
+    name:string,
+    age:number,
+    belt:string
+}
+ninjatwo={name:'mario',age:23,belt:'black' }
+
+//Dynamic Values
+let agv:any = 25;
+agv =true;
+console.log(agv)
+agv='hello'
+console.log(agv)
+
+let mix2: any[]=[];
+mix2.push(75);
+mix2.push('mario')
+mix2.push(false)
+console.log(mix2)
+
+let ninjas2: {name:any, age:any};
+ninjas2={name:'yoshi',age:25}
+console.log(ninjas2)
+ninjas2={name:25,age:'yoshi'}
+console.log(ninjas2)
+
+
+
